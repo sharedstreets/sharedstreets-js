68b80aa1bddab35406ba3382bbc778f949aeeb83
Denis <carriere.denis@gmail.com>
2017-12-29 18:29:30 -0500

    Update tests

--- test/geometry.test.ts
+++ test/geometry.test.ts
@@ -1,11 +1,10 @@
-import { lineString } from '@turf/helpers'
 import test from 'ava'
-import geometry from '../src/geometry'
+import * as sharedstreets from '../'
 
 test('sharedstreets -- geometry', (t) => {
   const start = [-74.003388, 40.634538]
   const end = [-74.004107, 40.63406]
   const bearing = 228.890377
 
-  t.is(geometry(start, end, bearing).id, 'NxPFkg4CrzHeFhwV7Uiq7K')
+  t.is(sharedstreets.geometry(start, end, bearing).id, 'NxPFkg4CrzHeFhwV7Uiq7K')
 })
