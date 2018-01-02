e27d8c1bd9127db40b22c60379290d9672274391
Denis <carriere.denis@gmail.com>
2018-01-02 14:48:59 -0500

    Add standard linting

--- test/intersection.test.js
+++ test/intersection.test.js
@@ -1,10 +1,27 @@
+const fs = require('fs')
+const path = require('path')
+const glob = require('glob')
+const write = require('write-json-file')
+const load = require('load-json-file')
 const test = require('tape')
 const sharedstreets = require('../index')
 
 test('sharedstreets -- intersection', t => {
-  const start = [-74.003388, 40.634538]
-  const end = [-74.004107, 40.63406]
+  const pt = [-74.003388, 40.634538]
 
-  t.equal(sharedstreets.intersection(start).id, '5gRJyF2MT5BBErTyEesQLC')
+  t.equal(sharedstreets.intersection(pt).id, '5gRJyF2MT5BBErTyEesQLC')
+  t.end()
+})
+
+test('sharedstreets -- intersectionPbf', t => {
+  glob.sync(path.join(__dirname, 'pbf', 'in', '*.intersection.pbf')).forEach(filepath => {
+    const {name, base} = path.parse(filepath)
+    const buffer = fs.readFileSync(filepath)
+    const result = sharedstreets.intersectionPbf(buffer)
+
+    const outfile = filepath.replace(path.join('pbf', 'in', base), path.join('pbf', 'out', name + '.json'))
+    if (process.env.REGEN) write.sync(outfile, result)
+    t.deepEqual(result, load.sync(outfile), name)
+  })
   t.end()
 })
