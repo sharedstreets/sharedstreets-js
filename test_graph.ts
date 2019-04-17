a376a87f5cb9fd94352e8a526bea47741b71e427
Kevin Webb <kpwebb@gmail.com>
2019-04-17 12:21:21 -0400

    bumped graph tests

--- test_graph.ts
+++ test_graph.ts
@@ -19,32 +19,32 @@ import { Graph, GraphMode } from "./src/graph";
 
 const test = require('tape');
  
-// test("sharedstreets -- graph test", async (t:any) => {
+test("sharedstreets -- graph test", async (t:any) => {
  
-//   var params = new TilePathParams();
-//   params.source = 'osm/planet-181224';
-//   params.tileHierarchy = 7;
+  var params = new TilePathParams();
+  params.source = 'osm/planet-181224';
+  params.tileHierarchy = 7;
 
-//   // test polygon (dc area)
-//   const content = fs.readFileSync('test/geojson/test_route.geojson');
-//   var lineIn:turfHelpers.FeatureCollection<turfHelpers.LineString> = JSON.parse(content.toLocaleString());
-//   var graph = new Graph(envelope(lineIn), params);
-//   await graph.buildGraph();
+  // test polygon (dc area)
+  const content = fs.readFileSync('test/geojson/test_route.geojson');
+  var lineIn:turfHelpers.FeatureCollection<turfHelpers.LineString> = JSON.parse(content.toLocaleString());
+  var graph = new Graph(envelope(lineIn), params);
+  await graph.buildGraph();
 
-//   //t.equal(graph.id, 'd626d5b0-0dec-3e6f-97ff-d9712228a282');
-//   //var results = await graph.match(lineIn.features[0]);
-//   //lineIn.features[0].geometry.coordinates.reverse();
-//   var results2 = await graph.match(lineIn.features[0]);
-//   console.log(JSON.stringify(results2));
-//   // for(var result of results) {
-//   //   console.log(JSON.stringify(result.toDebugView()));
-//   // }
+  //t.equal(graph.id, 'd626d5b0-0dec-3e6f-97ff-d9712228a282');
+  //var results = await graph.match(lineIn.features[0]);
+  //lineIn.features[0].geometry.coordinates.reverse();
+  var results2 = await graph.match(lineIn.features[0]);
+  console.log(JSON.stringify(results2));
+  // for(var result of results) {
+  //   console.log(JSON.stringify(result.toDebugView()));
+  // }
 
-//     console.log(JSON.stringify(results2.toDebugView()));
+    console.log(JSON.stringify(results2.toDebugView()));
 
-//   t.end();
+  t.end();
 
-//  });
+ });
 
 
 const readline = require('readline');
