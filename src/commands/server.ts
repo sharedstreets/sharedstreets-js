0ad8d25be5cb6faf767da9e1217435aad1899e02
indraneel <i.m.indraneel@gmail.com>
2019-07-15 15:09:57 -0400

    wip: match lines directly in server

--- src/commands/server.ts
+++ src/commands/server.ts
@@ -3,6 +3,9 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import Path from 'path';
 import { matchLines } from "./match";
+import { Graph, GraphMode } from "../graph";
+import envelope from "@turf/envelope";
+import { TilePathParams } from "../tiles";
 
 export default class Server extends Command {
     static description = "run a local server to access the SharedStreets match tool via a REST API"
@@ -23,7 +26,7 @@ async function server() {
 
     const app = express(); 
     app.use(bodyParser.text());
-    app.post("/match/geoms", (req, res) => {
+    app.post("/match/geoms", async (req, res) => {
         console.log("POST match/geoms,\nrequest:")
         // console.log(req.query);
         // console.log(req.body);
@@ -32,7 +35,16 @@ async function server() {
         console.log(query);
         console.log(body);
         // console.log(h);
-        matchLines({}, body, query);
+        // matchLines({}, body, query);
+        var params = new TilePathParams();
+        // params.source = flags['tile-source'];
+        params.source = null;
+        // params.tileHierarchy = flags['tile-hierarchy']
+        params.tileHierarchy = null;
+
+        const extent = envelope(body);
+        const matcher = new Graph(extent, params, GraphMode.CAR_ALL);
+        await matcher.buildGraph();
     });
     app.listen(3001, () =>  {
         console.log('Server running at: http://localhost:3001/');
