c23b9479734f9c680b4413fa032e208f581c2f20
morganherlocker <morgan.herlocker@gmail.com>
2020-03-16 12:19:19 -0700

    implement optional osrm dependency injection

--- test_graph.ts
+++ test_graph.ts
@@ -5,6 +5,7 @@ import * as glob from "glob";
 import * as path from "path";
 import * as sharedstreetsPbf from "sharedstreets-pbf";
 import * as sharedstreets from "./src/index";
+import * as OSRM from "osrm"
 
 import * as turfHelpers from '@turf/helpers';
 import envelope from '@turf/envelope';
@@ -18,9 +19,9 @@ import { polygon } from "@turf/envelope/node_modules/@turf/helpers";
 import { Graph, GraphMode } from "./src/graph";
 
 const test = require('tape');
- 
+
 test("sharedstreets -- graph test", async (t:any) => {
- 
+
   var params = new TilePathParams();
   params.source = 'osm/planet-181224';
   params.tileHierarchy = 7;
@@ -28,11 +29,11 @@ test("sharedstreets -- graph test", async (t:any) => {
   // test polygon (dc area)
   const content = fs.readFileSync('test/geojson/test_route.geojson');
   var lineIn:turfHelpers.FeatureCollection<turfHelpers.LineString> = JSON.parse(content.toLocaleString());
-  var graph = new Graph(envelope(lineIn), params);
+  var graph = new Graph(osrm, envelope(lineIn), params);
   await graph.buildGraph();
 
   t.equal(graph.id, 'd626d5b0-0dec-3e6f-97ff-d9712228a282');
-  
+
   var results = await graph.matchGeom(lineIn.features[0]);
   lineIn.features[0].geometry.coordinates.reverse();
   var results2 = await graph.matchGeom(lineIn.features[0]);
@@ -42,4 +43,3 @@ test("sharedstreets -- graph test", async (t:any) => {
   t.end();
 
  });
-
--- test_match.ts
+++ test_match.ts
@@ -5,6 +5,7 @@ import * as glob from "glob";
 import * as path from "path";
 import * as sharedstreetsPbf from "sharedstreets-pbf";
 import * as sharedstreets from "./src/index";
+import * as OSRM from "osrm"
 
 import * as turfHelpers from '@turf/helpers';
 
@@ -19,22 +20,22 @@ const test = require('tape');
 
 const BUILD_TEST_OUPUT = false;
 
-test("match points", async (t:any) => { 
+test("match points", async (t:any) => {
 
     // test polygon (dc area)
     const content = fs.readFileSync('test/geojson/points_1.in.geojson');
     var pointsIn:turfHelpers.FeatureCollection<turfHelpers.Point> = JSON.parse(content.toLocaleString());
     var cleanedPoints = new CleanedPoints(pointsIn);
-    
+
     var points:turfHelpers.FeatureCollection<turfHelpers.Point> = turfHelpers.featureCollection(cleanedPoints.clean);
- 
+
     var params = new TilePathParams();
     params.source = 'osm/planet-180430';
     params.tileHierarchy = 6;
- 
+
    // test matcher point candidates
-   var matcher = new Graph(null, params);
-   
+   var matcher = new Graph(osrm, null, params);
+
    var matchedPoints:turfHelpers.Feature<turfHelpers.Point>[] = [];
    for(let searchPoint of points.features) {
      let matches = await matcher.matchPoint(searchPoint, null, 3);
@@ -43,74 +44,74 @@ test("match points", async (t:any) => {
      }
    }
    const matchedPointFeatureCollection_1a:turfHelpers.FeatureCollection<turfHelpers.Point> = turfHelpers.featureCollection(matchedPoints);
-   
+
    const expected_1a_file = 'test/geojson/points_1a.out.geojson';
    if(BUILD_TEST_OUPUT) {
      var expected_1a_out:string = JSON.stringify(matchedPointFeatureCollection_1a);
      fs.writeFileSync(expected_1a_file, expected_1a_out);
    }
- 
+
    const expected_1a_in = fs.readFileSync(expected_1a_file);
    const expected_1a:turfHelpers.FeatureCollection<turfHelpers.Point> = JSON.parse(expected_1a_in.toLocaleString());
-   
+
    t.deepEqual(expected_1a, matchedPointFeatureCollection_1a);
- 
+
    matcher.searchRadius = 1000;
- 
+
    var matchedPoints:turfHelpers.Feature<turfHelpers.Point>[] = [];
    let matches = await matcher.matchPoint(points.features[0], null, 10);
- 
+
    for(let match of matches) {
      matchedPoints.push(match.toFeature());
    }
    const matchedPointFeatureCollection_1b:turfHelpers.FeatureCollection<turfHelpers.Point> = turfHelpers.featureCollection(matchedPoints);
-   
+
    const expected_1b_file = 'test/geojson/points_1b.out.geojson';
- 
+
    if(BUILD_TEST_OUPUT) {
      var expected_1b_out:{} = JSON.stringify(matchedPointFeatureCollection_1b);
      fs.writeFileSync(expected_1b_file, expected_1b_out);
    }
- 
+
    const expected_1b_in = fs.readFileSync(expected_1b_file);
    const expected_1b:{} = JSON.parse(expected_1b_in.toLocaleString());
-   
+
    t.deepEqual(expected_1b, matchedPointFeatureCollection_1b);
- 
-   
+
+
    t.end();
  });
- 
- 
- test("match lines 1", async (t:any) => { 
- 
+
+
+ test("match lines 1", async (t:any) => {
+
    // test polygon (dc area)
    const content = fs.readFileSync('test/geojson/sf_centerlines.sample.geojson');
    var linesIn:turfHelpers.FeatureCollection<turfHelpers.LineString> = JSON.parse(content.toLocaleString());
-   
-   var cleanedLines = new CleanedLines(linesIn);  
+
+   var cleanedLines = new CleanedLines(linesIn);
    var lines:turfHelpers.FeatureCollection<turfHelpers.LineString> = turfHelpers.featureCollection(cleanedLines.clean);
- 
+
    var params = new TilePathParams();
    params.source = 'osm/planet-180430';
    params.tileHierarchy = 6;
- 
+
   //test matcher point candidates
-  var matcher = new Graph(envelope(lines), params);
+  var matcher = new Graph(osrm, envelope(lines), params);
   await matcher.buildGraph();
-  
+
   var matchedLines = turfHelpers.featureCollection([]);
   for(var line of lines.features) {
     var pathCandidate = await matcher.matchGeom(line);
     matchedLines.features.push(pathCandidate.matchedPath);
   }
-  
+
   const expected_1a_file = 'test/geojson/sf_centerlines.sample.out.geojson';
   if(BUILD_TEST_OUPUT) {
     var expected_1a_out:string = JSON.stringify(matchedLines);
     fs.writeFileSync(expected_1a_file, expected_1a_out);
   }
- 
+
   const expected_1a_in = fs.readFileSync(expected_1a_file);
   const expected_1a:{} = JSON.parse(expected_1a_in.toLocaleString());
   t.deepEqual(matchedLines, expected_1a);
@@ -118,13 +119,13 @@ test("match points", async (t:any) => {
   t.end();
  });
 
- test("match lines 2 -- snapping and directed edges", async (t:any) => { 
- 
+ test("match lines 2 -- snapping and directed edges", async (t:any) => {
+
   // test polygon (dc area)
   const content = fs.readFileSync('test/geojson/line-directed-test.in.geojson');
   var linesIn:turfHelpers.FeatureCollection<turfHelpers.LineString> = JSON.parse(content.toLocaleString());
 
-  var cleanedLines = new CleanedLines(linesIn);  
+  var cleanedLines = new CleanedLines(linesIn);
   var lines:turfHelpers.FeatureCollection<turfHelpers.LineString> = turfHelpers.featureCollection(cleanedLines.clean);
 
   var params = new TilePathParams();
@@ -132,9 +133,9 @@ test("match points", async (t:any) => {
   params.tileHierarchy = 6;
 
  //test matcher point candidates
- var matcher = new Graph(envelope(lines), params);
+ var matcher = new Graph(osrm, envelope(lines), params);
  await matcher.buildGraph();
- 
+
  var matchedLines = turfHelpers.featureCollection([]);
  for(var line of lines.features) {
    var pathCandidate = await matcher.matchGeom(line);
@@ -147,7 +148,7 @@ test("match points", async (t:any) => {
    var expected_1a_out:string = JSON.stringify(matchedLines);
    fs.writeFileSync(expected_1a_file, expected_1a_out);
  }
- 
+
 
  const expected_1a_in = fs.readFileSync(expected_1a_file);
  const expected_1a:{} = JSON.parse(expected_1a_in.toLocaleString());
@@ -178,13 +179,13 @@ test("match points", async (t:any) => {
 
 
 
-// test("match grid", async (t:any) => { 
- 
+// test("match grid", async (t:any) => {
+
 //   // test polygon (dc area)
 //   const content = fs.readFileSync('test/geojson/sf_centerlines.sample.geojson');
 //   var linesIn:turfHelpers.FeatureCollection<turfHelpers.LineString> = JSON.parse(content.toLocaleString());
 
-//   var cleanedLines = new CleanedLines(linesIn);  
+//   var cleanedLines = new CleanedLines(linesIn);
 //   var lines:turfHelpers.FeatureCollection<turfHelpers.LineString> = turfHelpers.featureCollection(cleanedLines.clean);
 
 //   var params = new TilePathParams();
@@ -194,14 +195,14 @@ test("match points", async (t:any) => {
 //  //test matcher point candidates
 //  var matcher = new Graph(envelope(lines), params);
 //  await matcher.buildGraph();
- 
+
 //  var matchedLines = turfHelpers.featureCollection([]);
 //  for(var line of lines.features) {
-   
+
 //    var pathCandidate = await matcher.match(line);
 //    matchedLines.features.push(pathCandidate.matchedPath);
 //  }
- 
+
 
 
 //  const expected_1a_file = 'test/geojson/sf_centerlines.1a.out.geojson';
@@ -209,7 +210,7 @@ test("match points", async (t:any) => {
 //    var expected_1a_out:string = JSON.stringify(matchedLines);
 //    fs.writeFileSync(expected_1a_file, expected_1a_out);
 //  }
- 
+
 
 //  const expected_1a_in = fs.readFileSync(expected_1a_file);
 //  const expected_1a:{} = JSON.parse(expected_1a_in.toLocaleString());
@@ -239,13 +240,13 @@ test("match points", async (t:any) => {
 // });
 
 
-// test("match roundabout", async (t:any) => { 
- 
+// test("match roundabout", async (t:any) => {
+
 //   // test polygon (dc area)
 //   const content = fs.readFileSync('test/geojson/roundabout.1a.geojson');
 //   var linesIn:turfHelpers.FeatureCollection<turfHelpers.LineString> = JSON.parse(content.toLocaleString());
 
-//   var cleanedLines = new CleanedLines(linesIn);  
+//   var cleanedLines = new CleanedLines(linesIn);
 //   var lines:turfHelpers.FeatureCollection<turfHelpers.LineString> = turfHelpers.featureCollection(cleanedLines.clean);
 
 //   var params = new TilePathParams();
@@ -255,7 +256,7 @@ test("match points", async (t:any) => {
 //  //test matcher point candidates
 //  var matcher = new Graph(envelope(lines), params);
 //  await matcher.buildGraph();
- 
+
 //  var matchedLines = turfHelpers.featureCollection([]);
 //  for(var line of lines.features) {
 //    var pathCandidate = await matcher.match(line);
@@ -267,7 +268,7 @@ test("match points", async (t:any) => {
 //    var expected_1a_out:string = JSON.stringify(matchedLines);
 //    fs.writeFileSync(expected_1a_file, expected_1a_out);
 //  }
- 
+
 
 //  const expected_1a_in = fs.readFileSync(expected_1a_file);
 //  const expected_1a:{} = JSON.parse(expected_1a_in.toLocaleString());
@@ -298,14 +299,14 @@ test("match points", async (t:any) => {
 
 
 
-// test("match long paths", async (t:any) => { 
- 
+// test("match long paths", async (t:any) => {
+
 //   // test polygon (dc area)
 //   const content = fs.readFileSync('test/geojson/long-paths.geojson');
 //   var linesIn:turfHelpers.FeatureCollection<turfHelpers.LineString> = JSON.parse(content.toLocaleString());
 
 //   console.log("1");
-//   var cleanedLines = new CleanedLines(linesIn);  
+//   var cleanedLines = new CleanedLines(linesIn);
 //   var lines:turfHelpers.FeatureCollection<turfHelpers.LineString> = turfHelpers.featureCollection(cleanedLines.clean);
 
 //   var params = new TilePathParams();
@@ -316,7 +317,7 @@ test("match points", async (t:any) => {
 //  var matcher = new Graph(envelope(lines), params);
 //  matcher.searchRadius = 20;
 //  await matcher.buildGraph();
- 
+
 //  var matchedLines = turfHelpers.featureCollection([]);
 //  for(var line of lines.features) {
 //    if(line.properties['analysis_id'] == 1454853)
@@ -324,7 +325,7 @@ test("match points", async (t:any) => {
 //    var pathCandidate = await matcher.match(line);
 //    //matchedLines.features.push(pathCandidate.matchedPath);
 //  }
- 
+
 //  const BUILD_TEST_OUPUT = false;
 
 // //  const expected_1a_file = 'test/geojson/line-directed-test-snapped.out.geojson';
@@ -332,7 +333,7 @@ test("match points", async (t:any) => {
 // //    var expected_1a_out:string = JSON.stringify(matchedLines);
 // //    fs.writeFileSync(expected_1a_file, expected_1a_out);
 // //  }
- 
+
 
 // //  const expected_1a_in = fs.readFileSync(expected_1a_file);
 // //  const expected_1a:{} = JSON.parse(expected_1a_in.toLocaleString());
@@ -362,14 +363,14 @@ test("match points", async (t:any) => {
 // });
 
 
-// test("match long paths", async (t:any) => { 
- 
+// test("match long paths", async (t:any) => {
+
 //   // test polygon (dc area)
 //   const content = fs.readFileSync('test/geojson/expressways.geojson');
 //   var linesIn:turfHelpers.FeatureCollection<turfHelpers.LineString> = JSON.parse(content.toLocaleString());
 
 //   console.log("1");
-//   var cleanedLines = new CleanedLines(linesIn);  
+//   var cleanedLines = new CleanedLines(linesIn);
 //   var lines:turfHelpers.FeatureCollection<turfHelpers.LineString> = turfHelpers.featureCollection(cleanedLines.clean);
 
 //   var params = new TilePathParams();
@@ -381,7 +382,7 @@ test("match points", async (t:any) => {
 //  matcher.graphMode = GraphMode.CAR_MOTORWAY_ONLY;
 //  matcher.searchRadius = 20;
 //  await matcher.buildGraph();
- 
+
 //  var matchedLines = turfHelpers.featureCollection([]);
 //  for(var line of lines.features) {
 //    if(line.properties['analysis_id'] == 1454853)
@@ -389,7 +390,7 @@ test("match points", async (t:any) => {
 //    var pathCandidate = await matcher.match(line);
 //    //matchedLines.features.push(pathCandidate.matchedPath);
 //  }
- 
+
 //  const BUILD_TEST_OUPUT = false;
 
 // //  const expected_1a_file = 'test/geojson/line-directed-test-snapped.out.geojson';
@@ -397,7 +398,7 @@ test("match points", async (t:any) => {
 // //    var expected_1a_out:string = JSON.stringify(matchedLines);
 // //    fs.writeFileSync(expected_1a_file, expected_1a_out);
 // //  }
- 
+
 
 // //  const expected_1a_in = fs.readFileSync(expected_1a_file);
 // //  const expected_1a:{} = JSON.parse(expected_1a_in.toLocaleString());
@@ -425,4 +426,3 @@ test("match points", async (t:any) => {
 
 //  t.end();
 // });
-
